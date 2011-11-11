184b58ae358dbc69cd7b5153b1a0f84ddb28e24e
Sebastian Staudt <koraktor@gmail.com>
2011-11-11 09:04:50 +0100

    Added specs for the compressor

---
+++ spec/compressor_spec.rb
@@ -0,0 +1,31 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2011, Sebastian Staudt
+
+require 'helper'
+
+describe RBzip2::Compressor do
+
+  before do
+    @txt_file = File.new File.join(File.dirname(__FILE__), 'fixtures/test.txt')
+    @bz2_file  = File.new File.join(File.dirname(__FILE__), 'fixtures/test.bz2')
+    @io = StringIO.new
+    @bz2_compressor = RBzip2::Compressor.new @io
+  end
+
+  it 'acts like a standard IO' do
+    methods = RBzip2::Compressor.instance_methods.map { |m| m.to_sym }
+    methods.should include(:write, :close)
+  end
+
+  it 'should be able to compress raw data' do
+    @bz2_compressor.write @txt_file.read
+    @bz2_compressor.close
+
+    eq_bz2 = eq @bz2_file.read
+    eq_bz2.instance_variable_set :@diffable, false
+    @io.string.should eq_bz2
+  end
+
+end
