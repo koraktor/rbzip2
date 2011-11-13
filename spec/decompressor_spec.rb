1b4c004a590670bd30c134457c8a47419654b0c9
Sebastian Staudt <koraktor@gmail.com>
2011-11-13 22:43:52 +0100

    Restructured Decompressor specs

--- spec/decompressor_spec.rb
+++ spec/decompressor_spec.rb
@@ -7,35 +7,39 @@ require 'helper'
 
 describe RBzip2::Decompressor do
 
-  before do
-    @txt_file = File.new File.join(File.dirname(__FILE__), 'fixtures/test.txt')
-    bz2_file  = File.new File.join(File.dirname(__FILE__), 'fixtures/test.bz2')
-    @bz2_decompressor = RBzip2::Decompressor.new bz2_file
-  end
-
   it 'acts like a standard IO' do
     methods = RBzip2::Decompressor.instance_methods.map { |m| m.to_sym }
     methods.should include(:read, :close)
   end
 
   it 'knows its size' do
-    @bz2_decompressor.size.should be(375)
+    bz2_file = File.new File.join(File.dirname(__FILE__), 'fixtures/test.bz2')
+    bz2_decompressor = RBzip2::Decompressor.new bz2_file
+
+    bz2_decompressor.size.should == 375
   end
 
   it 'knows the size of the uncompressed data' do
-    @bz2_decompressor.uncompressed.should be(704)
+    bz2_file = File.new File.join(File.dirname(__FILE__), 'fixtures/test.bz2')
+    bz2_decompressor = RBzip2::Decompressor.new bz2_file
+
+    bz2_decompressor.uncompressed.should == 704
   end
 
   it 'should be able to decompress compressed data' do
-    @bz2_decompressor.read.should eq(@txt_file.read)
+    bz2_file = File.new File.join(File.dirname(__FILE__), 'fixtures/test.bz2')
+    bz2_decompressor = RBzip2::Decompressor.new bz2_file
+    txt_file = File.new File.join(File.dirname(__FILE__), 'fixtures/test.txt')
+
+    bz2_decompressor.read.should eq(txt_file.read)
   end
 
   it 'should be able to decompress large compressed data' do
     txt_file = File.new File.join(File.dirname(__FILE__), 'fixtures/big_test.txt')
-    bz2_file  = File.new File.join(File.dirname(__FILE__), 'fixtures/big_test.bz2')
-    @bz2_decompressor = RBzip2::Decompressor.new bz2_file
+    bz2_file = File.new File.join(File.dirname(__FILE__), 'fixtures/big_test.bz2')
+    bz2_decompressor = RBzip2::Decompressor.new bz2_file
 
-    @bz2_decompressor.read.should eq(txt_file.read)
+    bz2_decompressor.read.should eq(txt_file.read)
   end
 
 end
