56f20aeb1fb01e7de9687ee3827e117c286ddfa5
Sebastian Staudt <koraktor@gmail.com>
2013-07-03 17:51:24 +0200

    Simplify IO extension

--- lib/core_ext/io.rb
+++ lib/core_ext/io.rb
@@ -1,16 +1,12 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2011, Sebastian Staudt
+# Copyright (c) 2011-2013, Sebastian Staudt
 
 unless IO.method_defined? :readbyte
 
-  class IO
-
-    def readbyte
-      read(1)[0].ord
-    end
-
+  def IO.readbyte
+    read(1)[0].ord
   end
 
 end
